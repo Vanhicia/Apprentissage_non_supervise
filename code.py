535cad17b1b747818c1445d47305c9f47a936bd7
KimiNako <KimiNako@users.noreply.github.com>
2019-11-26 09:47:55 +0100

    Corret typo main code

--- code.py
+++ code.py
@@ -6,14 +6,16 @@ data_bruite = dataset_bruite[0]['CLASS']
 
 
 dataset_connexe = arff.loadarff(open(path + 'connexe_petites_taches.arff', 'r'))
-data_connexe = dataset_connexe[0]['CALSS']
+
+data_connexe = dataset_connexe[0]['class']
 
 dataset_non_connexe = arff.loadarff(open(path + 'non_connexe.arff', 'r'))
-data_non_connexe = dataset_non_connexe[0]['CALSS']
+data_non_connexe = dataset_non_connexe[0]['class']
 
 
-dataset_densite_variable = arff.loadarff(open(path + 'non_densite_variable.arff', 'r'))
-data_densite_variable = dataset_densite_variable[0]['CALSS']
+dataset_densite_variable = arff.loadarff(open(path + 'densite_variable.arff', 'r'))
+print(dataset_densite_variable)
+data_densite_variable = dataset_densite_variable[0]['class']
 
 dataset_densite_non_variable = dataset_non_connexe
-data_densite_variable = dataset_densite_non_variable[0]['CALSS']
+data_densite_non_variable = dataset_densite_non_variable[0]['class']
