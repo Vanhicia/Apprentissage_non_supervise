a191888070b0279ab77b8309cdf5a90f6c7232b7
Vanhicia <vanhulle@etud.insa-toulouse.fr>
2019-11-26 12:12:26 +0100

    add indices

--- code.py
+++ code.py
@@ -1,40 +1,33 @@
+from scipy.io import arff
+from sklearn import cluster
+from sklearn import metrics
 import numpy as np
 from matplotlib import pyplot as plt
-from scipy.io import arff
-from sklearn.cluster import KMeans
 
+list_dataset = ["connexe_petites_taches", "bruite","non_connexe", "densite_variable", "mal_separe"]
 path = "./data/"
 
-
-dataset_bruite = arff.loadarff(open(path + 'bruite.arff', 'r'))
-data_bruite = dataset_bruite[0]
-
-
-
-#print(val)
-#plt.scatter(data_bruite['x'], data_bruite['y'])
-#plt.show()
-
-dataset_convexe = arff.loadarff(open(path + 'connexe_petites_taches.arff', 'r'))
-data_convexe = dataset_convexe[0]
-
-val = [[x[0],x[1]] for x in data_convexe]
- 
-dataset_non_convexe = arff.loadarff(open(path + 'non_connexe.arff', 'r'))
-data_non_convexe = dataset_non_convexe[0]['class']
-
-
-dataset_densite_variable = arff.loadarff(open(path + 'densite_variable.arff', 'r'))
-data_densite_variable = dataset_densite_variable[0]['class']
-
-dataset_densite_non_variable = dataset_non_convexe
-data_densite_non_variable = dataset_densite_non_variable[0]['class']
-
 print("K_Means")
-kmeans = KMeans(n_clusters=30, init='k-means++')
-kmeans.fit(val)
-y_pred = kmeans.predict(val)
-print("Score")
-print(kmeans.score(val))
-plt.scatter(data_convexe['x'], data_convexe['y'], c=y_pred)
-plt.show()
+k = [30, 2, 4, 10]
+name = ["connexe_petites_taches", "bruite", "densite_variable", "mal_separe"]
+
+for i in range(4):
+    print(name[i])
+    dataset = arff.loadarff(open(path + name[i] + '.arff', 'r'))
+    data = [[x[0],x[1]] for x in dataset[0]]
+
+    kmeans = cluster.KMeans(n_clusters=k[i], init='k-means++')
+    kmeans.fit(data)
+    y_pred = kmeans.predict(data)
+    print("Score")
+    print(kmeans.score(data))
+    
+    labels = kmeans.labels_
+    print("Indice de Davies-Bouldin")
+    print(metrics.davies_bouldin_score(data, labels))
+    
+    print("Coefficient de silhouette")
+    print(metrics.silhouette_score(data, labels, metric='euclidean'))
+
+    plt.scatter((dataset[0])['x'], (dataset[0])['y'], c=y_pred)
+    plt.show()
